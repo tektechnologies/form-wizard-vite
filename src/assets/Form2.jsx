2e73667920c4bc3a9ece2e655e297939a3b37825
tektechnologies <info@tektechnologies.com>
2024-05-31 20:00:02 -0500

    add dev tools

--- src/assets/Form2.jsx
+++ src/assets/Form2.jsx
@@ -1,15 +1,19 @@
 // Form2.js
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const Form2 = ({ onSubmit, defaultValues }) => {
-  const { register, handleSubmit } = useForm({ defaultValues });
+  const { register, handleSubmit, control } = useForm({ defaultValues });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('field2')} placeholder="Field 2" />
-      <button type="submit">Next</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("field2")} placeholder="Field 2" />
+        <button type="submit">Next</button>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
 
--- src/assets/Form3.jsx
+++ src/assets/Form3.jsx
@@ -1,15 +1,19 @@
 // Form3.js
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const Form3 = ({ onSubmit, defaultValues }) => {
-  const { register, handleSubmit } = useForm({ defaultValues });
+  const { register, handleSubmit, control } = useForm({ defaultValues });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('field3')} placeholder="Field 3" />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("field3")} placeholder="Field 3" />
+        <button type="submit">Submit</button>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
 
