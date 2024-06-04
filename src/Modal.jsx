dce5cc2e6ff75cd9a4073b2c26b2fa75e1c65c2a
tektechnologies <info@tektechnologies.com>
2024-06-04 12:12:15 -0500

    resolve lost curly brace

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -6,77 +6,78 @@ import Form3 from './assets/Form3';
 const Modal = () => {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
-  firstName : '',
-  lastName :  '',
-  companyName :  '',
-  emailAddress : '',
-  phoneNumber :  '',
-  visitForeignCountry :  '',
-  visitReason :  '',
-  toolsRequired : '',
-  toolReason :  '',
-  visitDate :  '',
-  startTime :  '',
-  endTime :  '',
-  userLocation :  '',
-  productcategory :  '',
-  //second form 
-  field2: '',
-  //third form
-  field3: '',
+    firstName: '',
+    lastName: '',
+    companyName: '',
+    emailAddress: '',
+    phoneNumber: '',
+    visitForeignCountry: '',
+    visitReason: '',
+    toolsRequired: '',
+    toolReason: '',
+    visitDate: '',
+    startTime: '',
+    endTime: '',
+    userLocation: '',
+    productcategory: '',
+    //second form 
+    field2: '',
+    //third form
+    field3: '',
   });
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
-    });
-  }, []);
-  
+  // useEffect(() => {
+  //   console.log('geooooo');
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     console.log("Latitude is :", position.coords.latitude);
+  //     console.log("Longitude is :", position.coords.longitude);
+  //   });
+  // }, []);
+
   const handleNext = (data) => {
     console.log('handleNext data ', data);
-  // https://typeofnan.dev/fix-the-maximum-update-depth-exceeded-error-in-react/
-  const handleNext = ((data) => {
-      console.log('clickin : ',data);
+    // https://typeofnan.dev/fix-the-maximum-update-depth-exceeded-error-in-react/
+    const handleNext = ((data) => {
+      console.log('clickin : ', data);
 
-    setFormData(prevFormData => ({ ...prevFormData, ...data }));
-    
-    setStep(step + 1);
-});
+      setFormData(prevFormData => ({ ...prevFormData, ...data }));
 
-  const handleBack = () => {
-    setStep(step - 1);
-  };
+      setStep(step + 1);
+    });
 
-  const handleFinalSubmit = (data) => {
-    console.log('handle Final Submit:FINAL', data);
-    setFormData(prevFormData => ({ ...prevFormData, ...data }));
-    // handle final submission
-    // console.log("Final Data:", { ...formData, ...data });
-    
-  };
+    const handleBack = () => {
+      setStep(step - 1);
+    };
 
-  // console.log('state : ',formData);
-  return (
-    <div className="modal">
-     {/* updating state incase user back clicks  */}
-      {step === 1 && <Form1 onSubmit={handleNext}  defaultValues={formData} />}
-      {step === 2 && (
-        <div>
+    const handleFinalSubmit = (data) => {
+      console.log('handle Final Submit:FINAL', data);
+      setFormData(prevFormData => ({ ...prevFormData, ...data }));
+      // handle final submission
+      // console.log("Final Data:", { ...formData, ...data });
+
+    };
+
+    // console.log('state : ',formData);
+    return (
+      <div className="modal">
         {/* updating state incase user back clicks  */}
-          <Form2 onSubmit={handleNext} defaultValues={formData} handleBack={handleBack} />
-          {/* <button onClick={handleBack}>Back</button> */}
-        </div>
-      )}
-      {step === 3 && (
-        <div>
-        {/* manages submitting all data in state on final submit  */}
-          <Form3 onSubmit={handleFinalSubmit} defaultValues={formData} handleBack={handleBack} />
-          {/* <button onClick={handleBack}>Back</button> */}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Modal;
+        {step === 1 && <Form1 onSubmit={handleNext} defaultValues={formData} />}
+        {step === 2 && (
+          <div>
+            {/* updating state incase user back clicks  */}
+            <Form2 onSubmit={handleNext} defaultValues={formData} handleBack={handleBack} />
+            {/* <button onClick={handleBack}>Back</button> */}
+          </div>
+        )}
+        {step === 3 && (
+          <div>
+            {/* manages submitting all data in state on final submit  */}
+            <Form3 onSubmit={handleFinalSubmit} defaultValues={formData} handleBack={handleBack} />
+            {/* <button onClick={handleBack}>Back</button> */}
+          </div>
+        )}
+      </div>
+    );
+  };
+}
+  export default Modal;
