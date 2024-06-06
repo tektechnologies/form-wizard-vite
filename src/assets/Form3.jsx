9372218465553834260dfce69856918d5bb29d95
tektechnologies <info@tektechnologies.com>
2024-06-06 11:05:40 -0500

    remove svg input to replace with form inputs

--- src/assets/Form3.jsx
+++ src/assets/Form3.jsx
@@ -9,19 +9,18 @@ const Form3 = ({ onSubmit, defaultValues, handleBack }) => {
   return (
     <>
       <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
-        {/* <input {...register("field3")} placeholder="Field 3" /> */}
+       
         <object type="image/svg+xml" data="file1.svg" >
           <span className="errorSpan" >Your browser does not support SVG!</span>
         </object>
-
+ {/* <input {...register("field3")} placeholder="Field 3" /> */}
 
         <h2>Signature</h2>
         <div className="signature-div">
           <p>Tap To Sign</p>
         </div>
-        <div>
+        <div className="button-group">
           <button onClick={handleBack} className="back-button-50">Back</button>
-
           <button type="submit" className="next-button-50">Submit</button>
         </div>
       </form>
