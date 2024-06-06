61f22c36376cfe491eab02db81628e22458df0d5
tektechnologies <info@tektechnologies.com>
2024-06-06 14:02:20 -0500

    add required message attribute to checkbox

--- src/assets/Form2.jsx
+++ src/assets/Form2.jsx
@@ -4,8 +4,8 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 const Form2 = ({ onSubmit, defaultValues, handleBack }) => {
-  const { register, handleSubmit, control } = useForm({ defaultValues });
-
+  const { register, handleSubmit, control, formState } = useForm({ defaultValues });
+  const { errors } = formState;
 
   return (
     <>
@@ -24,12 +24,15 @@ const Form2 = ({ onSubmit, defaultValues, handleBack }) => {
           <input
             type="checkbox"
             id="animalWelfareInput"
-            {...register("animalWelfareInput")}
+            {...register("animalWelfareInput", {
+              required: 'Im gonna need you to check this box, mmmmk.'
+            })}
           />
           I certify, by signing the above statement, that if at any time during my visit I witness signs of animal abuse of
           neglect I will report the incident immediately to Door Access System or via the
           Door Access System Animal Welfare Hotline. If I have any questions regarding this information or something I saw, I will speak to a member of Door Access System within 24 hours.
         </label>
+        <p>{errors.animalWelfareInput?.message}</p>
         <h2>Signature</h2>
         <div className="signature-div">
           <p>Tap To Sign</p>
