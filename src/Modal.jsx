b0e656fc28dcc3b0ae2a3ef879e9245fce5108b5
tektechnologies <info@tektechnologies.com>
2024-06-06 10:41:20 -0500

    remove geo location method

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -6,47 +6,39 @@ import Form3 from './assets/Form3';
 const Modal = () => {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
-  firstName : 'c',
-  lastName :  'cc',
-  companyName :  'c',
-  emailAddress : 'bob@test.com',
-  phoneNumber :  '555-555-5555',
-  visitForeignCountry :  false,
-  visitReason :  '',
-  toolsRequired : false,
-  toolReason :  '',
-  visitDate :  '',
-  startTime :  '',
-  endTime :  '',
-  userLocation :  '',
-  productcategory :  '',
-  //second form 
-  field2: '',
-  //third form
-  field3: '',
+    firstName: 'c',
+    lastName: 'cc',
+    companyName: 'c',
+    emailAddress: 'bob@test.com',
+    phoneNumber: '555-555-5555',
+    visitForeignCountry: false,
+    visitReason: '',
+    toolsRequired: false,
+    toolReason: '',
+    visitDate: '',
+    startTime: '',
+    endTime: '',
+    userLocation: '',
+    productcategory: '',
+    //second form 
+    field2: '',
+    //third form
+    field3: '',
   });
 
 
 
   // https://typeofnan.dev/fix-the-maximum-update-depth-exceeded-error-in-react/
   const handleNext = ((data) => {
-      console.log('state on NEXT : ',data);
+    console.log('state on NEXT : ', data);
 
     setFormData(prevFormData => ({ ...prevFormData, ...data }));
-    
+
     setStep(step + 1);
-});
+  });
 
 
-// https://stackoverflow.com/questions/3397585/navigator-geolocation-getcurrentposition-sometimes-works-sometimes-doesnt
-//                     based on location of IP address
-  useEffect(() => {
-    // console.log('geooooo');
-    navigator.geolocation.getCurrentPosition(function(position) {
-      // console.log("Latitude is :", position.coords.latitude);
-      // console.log("Longitude is :", position.coords.longitude);
-    });
-  }, []);
+
 
 
   const handleBack = () => {
@@ -58,24 +50,24 @@ const Modal = () => {
     setFormData(prevFormData => ({ ...prevFormData, ...data }));
     // handle final submission
     // console.log("Final Data:", { ...formData, ...data });
-    
+
   };
 
   // console.log('state : ',formData);
   return (
     <div className="modal">
-     {/* updating state incase user back clicks  */}
-      {step === 1 && <Form1 onSubmit={handleNext}  defaultValues={formData} />}
+      {/* updating state incase user back clicks  */}
+      {step === 1 && <Form1 onSubmit={handleNext} defaultValues={formData} />}
       {step === 2 && (
         <div>
-        {/* updating state incase user back clicks  */}
+          {/* updating state incase user back clicks  */}
           <Form2 onSubmit={handleNext} defaultValues={formData} handleBack={handleBack} />
           {/* <button onClick={handleBack}>Back</button> */}
         </div>
       )}
       {step === 3 && (
         <div>
-        {/* manages submitting all data in state on final submit  */}
+          {/* manages submitting all data in state on final submit  */}
           <Form3 onSubmit={handleFinalSubmit} defaultValues={formData} handleBack={handleBack} />
           {/* <button onClick={handleBack}>Back</button> */}
         </div>
