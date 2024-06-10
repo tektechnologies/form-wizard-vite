6077c830a4b754bee995a4d10af27e780d4dcd66
tektechnologies <info@tektechnologies.com>
2024-06-10 14:36:27 -0500

    add form context update modal comp

---
+++ src/FormContext.jsx
@@ -0,0 +1,46 @@
+// FormContext.js
+import React, { createContext, useState } from 'react';
+
+export const FormContext = createContext();
+
+export const FormProvider = ({ children }) => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    companyName: '',
+    emailAddress: '',
+    phoneNumber: '',
+    visitForeignCountry: false,
+    visitReason: '',
+    toolsRequired: false,
+    toolReason: '',
+    visitDate: '',
+    startTime: '',
+    endTime: '',
+    userLocation: '',
+    productcategory: '',
+    animalWelfareInput: false,
+    bioSecurityBreifing: false,
+  });
+
+  const handleNext = (data) => {
+    setFormData((prevFormData) => ({ ...prevFormData, ...data }));
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleBack = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  const handleFinalSubmit = (data) => {
+    console.log('handle Final Submit:FINAL', data);
+    setFormData((prevFormData) => ({ ...prevFormData, ...data }));
+  };
+
+  return (
+    <FormContext.Provider value={{ step, formData, handleNext, handleBack, handleFinalSubmit }}>
+      {children}
+    </FormContext.Provider>
+  );
+};
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,59 +1,63 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { FormProvider, FormContext } from './FormContext';
+
 import Form1 from './assets/Form1';
 import Form2 from './assets/Form2';
 import Form3 from './assets/Form3';
 
 const Modal = () => {
-  const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    companyName: '',
-    emailAddress: '',
-    phoneNumber: '',
-    visitForeignCountry: false,
-    visitReason: '',
-    toolsRequired: false,
-    toolReason: '',
-    visitDate: '',
-    startTime: '',
-    endTime: '',
-    userLocation: '',
-    productcategory: '',
-    animalWelfareInput: false,
-    bioSecurityBreifing: false,
-  });
+  // const [step, setStep] = useState(1);
+  // const [formData, setFormData] = useState({
+  //   firstName: '',
+  //   lastName: '',
+  //   companyName: '',
+  //   emailAddress: '',
+  //   phoneNumber: '',
+  //   visitForeignCountry: false,
+  //   visitReason: '',
+  //   toolsRequired: false,
+  //   toolReason: '',
+  //   visitDate: '',
+  //   startTime: '',
+  //   endTime: '',
+  //   userLocation: '',
+  //   productcategory: '',
+  //   animalWelfareInput: false,
+  //   bioSecurityBreifing: false,
+  // });
+  // const handleNext = ((data) => {
+  //   setFormData(prevFormData => ({ ...prevFormData, ...data }));
+  //   setStep(prevStep => prevStep + 1);
+  // });
 
+  // const handleBack = () => {
+  //   setStep(prevStep => prevStep - 1);
+  // };
 
-
-  const handleNext = ((data) => {
-    setFormData(prevFormData => ({ ...prevFormData, ...data }));
-    setStep(prevStep => prevStep + 1);
-  });
-
-  const handleBack = () => {
-    setStep(prevStep => prevStep - 1);
-  };
-
-  const handleFinalSubmit = (data) => {
-    console.log('handle Final Submit:FINAL', data);
-    setFormData(prevFormData => ({ ...prevFormData, ...data }));
-  };
+  // const handleFinalSubmit = (data) => {
+  //   console.log('handle Final Submit:FINAL', data);
+  //   setFormData(prevFormData => ({ ...prevFormData, ...data }));
+  // };
 
   return (
-    <div className="modal">
-      {step === 1 && <Form1 onSubmit={handleNext} defaultValues={formData} />}
-      {step === 2 && (
-        <div>
-          <Form2 onSubmit={handleNext} defaultValues={formData} handleBack={handleBack} />
-        </div>
-      )}
-      {step === 3 && (
-        <div>
-          <Form3 onSubmit={handleFinalSubmit} defaultValues={formData} handleBack={handleBack} />
-        </div>
-      )}
-    </div>
+    // <div className="modal">
+    //   {step === 1 && <Form1 onSubmit={handleNext} defaultValues={formData} />}
+    //   {step === 2 && (
+    //     <div>
+    //       <Form2 onSubmit={handleNext} defaultValues={formData} handleBack={handleBack} />
+    //     </div>
+    //   )}
+    //   {step === 3 && (
+    //     <div>
+    //       <Form3 onSubmit={handleFinalSubmit} defaultValues={formData} handleBack={handleBack} />
+    //     </div>
+    //   )}
+    // </div>
+    <>
+
+
+    </>
   );
 };
 
