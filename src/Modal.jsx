3d74a1d94b00418043cdaa263b38ac8a89a86485
tektechnologies <info@tektechnologies.com>
2024-06-13 12:33:28 -0500

    add useEffect for scroll behavior so render forms load from top of page

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { FormProvider, FormContext } from "./FormContext";
 import Form1 from "./assets/Form1";
 import Form2 from "./assets/Form2";
@@ -6,6 +6,11 @@ import Form3 from "./assets/Form3";
 
 const Modal = () => {
   const { step } = useContext(FormContext);
+  
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [step]);
 
   return (
     <div className="modal">
